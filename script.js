57e3fdb5cce3dd9b06c31997f1eb5adfd5bcf457
mseneoudom <matt_sen@live.ca>
2020-04-12 02:19:33 -0400

    fixed cart number on mobile

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-//Scroll to section
-$(".headerScrollButton").click(function() {
-    $("body,html").animate(
-        {
-        scrollTop: $("section").offset().top
-        },
-        800 //speed
-    );
-});
+$(function(){
+    //Scroll to section
+    $(".headerScrollButton").click(function() {
+        $("body,html").animate(
+            {
+            scrollTop: $("section").offset().top
+            },
+            800 //speed
+        );
+    });
+})
